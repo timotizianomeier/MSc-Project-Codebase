789cc6356279e3723ee8af30f14fec4cc85f12e2
ttm25 <ttm25@imperial.ac.uk>
2026-06-11 18:30:25 +0100

    started setting up Python SDK

--- experiments/fer-2013/main.py
+++ experiments/fer-2013/main.py
@@ -14,3 +14,122 @@ print(train_set.classes)
 print(train_set.class_to_idx)
 print(len(train_set))
 print(len(test_set))
+
+# Based on the emotion recognition architecture that Amato et al. proposed
+class CNN(nn.Module):
+    def __init__(self, input_channel=1, output_channel=1, num_filter=16):
+        super(CNN, self).__init__()
+
+        # Encoder path
+        n = num_filter  # 16
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(input_channel, n, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU(),
+            nn.Conv2d(n, n, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU()
+        )
+
+        n *= 2  # 32
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(int(n / 2), n, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU(),
+            nn.Conv2d(n, n, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU()
+        )
+
+        n *= 2  # 64
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(int(n / 2), n, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU(),
+            nn.Conv2d(n, n, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU()
+        )
+
+        n *= 2  # 128
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(int(n / 2), n, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU(),
+            nn.Conv2d(n, n, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU()
+        )
+
+        # Decoder path
+        ### Insert your code ###
+        n //= 2  # 64
+        self.conv5_transposed = nn.ConvTranspose2d(int(n * 2), n, kernel_size=2, stride=2)
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(int(n * 2), n, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU(),
+            nn.Conv2d(n, n, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU()
+        )
+
+        n //= 2  # 32
+        self.conv6_transposed = nn.ConvTranspose2d(int(n * 2), n, kernel_size=2, stride=2)
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(int(n * 2), n, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU(),
+            nn.Conv2d(n, n, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU()
+        )
+
+        n //= 2  # 16
+        self.conv7_transposed = nn.ConvTranspose2d(int(n * 2), n, kernel_size=2, stride=2)
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(int(n * 2), n, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU(),
+            nn.Conv2d(n, n, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n),
+            nn.ReLU()
+        )
+
+        self.conv8 = nn.Conv2d(n, output_channel, kernel_size=1)
+
+        ### End of your code ###
+
+    def forward(self, x):
+        # Use the convolutional operators defined above to build the U-net
+        # The encoder part is already done for you.
+        # You need to complete the decoder part.
+        # Encoder
+        x = self.conv1(x)
+        conv1_skip = x
+
+        x = self.conv2(x)
+        conv2_skip = x
+
+        x = self.conv3(x)
+        conv3_skip = x
+
+        x = self.conv4(x)
+
+        # Decoder
+        ### Insert your code ###
+        x = self.conv5_transposed(x)
+        x = torch.cat((x, conv3_skip), dim=1)
+        x = self.conv5(x)
+
+        x = self.conv6_transposed(x)
+        x = torch.cat((x, conv2_skip), dim=1)
+        x = self.conv6(x)
+
+        x = self.conv7_transposed(x)
+        x = torch.cat((x, conv1_skip), dim=1)
+        x = self.conv7(x)
+
+        x = self.conv8(x)
+        ### End of your code ###
+        return x
